51d524eac1c525a94dc469ad97bec7932f091222
Hugo SEQUIER <60607241+Hugo-SEQUIER@users.noreply.github.com>
2022-03-19 00:12:45 +0100

    Tests fonctionnels sur n nombre de test

--- src/test/Tests.scala
+++ src/test/Tests.scala
@@ -7,7 +7,7 @@ import scala.util.Sorting
 class Tests extends AnyFlatSpec {
 
   "BestHand Carre" should "be defined" in {
-    val l = List[Cartes](Cartes.P(Valeur.CINQ,Couleur.CARREAU),Cartes.P(Valeur.CINQ,Couleur.COEUR),Cartes.P(Valeur.CINQ,Couleur.TREFLE),Cartes.P(Valeur.CINQ,Couleur.PIQUE),Cartes.P(Valeur.SIX,Couleur.CARREAU))
+    val l = List[Cartes](Cartes.P(Valeur.CINQ,Couleur.CARREAU),Cartes.P(Valeur.CINQ,Couleur.COEUR),Cartes.P(Valeur.CINQ,Couleur.TREFLE),Cartes.P(Valeur.CINQ,Couleur.PIQUE),Cartes.P(Valeur.SIX,Couleur.CARREAU),Cartes.P(Valeur.SIX,Couleur.COEUR),Cartes.P(Valeur.SIX,Couleur.PIQUE))
 
     val corre = List[Cartes](Cartes.P(Valeur.CINQ,Couleur.CARREAU),Cartes.P(Valeur.CINQ,Couleur.COEUR),Cartes.P(Valeur.CINQ,Couleur.TREFLE),Cartes.P(Valeur.CINQ,Couleur.PIQUE))
 
@@ -17,39 +17,45 @@ class Tests extends AnyFlatSpec {
   }
 
   "BestHand RoyalFlush" should "be defined" in {
-    val l = List[Cartes](Cartes.P(Valeur.DIX,Couleur.CARREAU),Cartes.P(Valeur.VALET,Couleur.CARREAU),Cartes.P(Valeur.DAME,Couleur.CARREAU),Cartes.P(Valeur.ROI,Couleur.CARREAU),Cartes.P(Valeur.AS,Couleur.CARREAU))
+    val l = List[Cartes](Cartes.P(Valeur.DIX,Couleur.CARREAU),Cartes.P(Valeur.VALET,Couleur.CARREAU),Cartes.P(Valeur.DAME,Couleur.CARREAU),Cartes.P(Valeur.ROI,Couleur.CARREAU),Cartes.P(Valeur.AS,Couleur.CARREAU),Cartes.P(Valeur.ROI,Couleur.PIQUE),Cartes.P(Valeur.AS,Couleur.PIQUE))
 
-    isQuinteFlushRoyale(l,Couleur.CARREAU,-1,0) shouldBe true
+    val lRES = List[Cartes](Cartes.P(Valeur.DIX,Couleur.CARREAU),Cartes.P(Valeur.VALET,Couleur.CARREAU),Cartes.P(Valeur.DAME,Couleur.CARREAU),Cartes.P(Valeur.ROI,Couleur.CARREAU),Cartes.P(Valeur.AS,Couleur.CARREAU))
+
     bestHand(l).length shouldBe 5
-    bestHand(l) shouldBe l
+    bestHand(l) shouldBe lRES
   }
 
   "BestHand QuinteFlush" should "be defined" in {
-    val l = List[Cartes](Cartes.P(Valeur.SEPT, Couleur.CARREAU), Cartes.P(Valeur.HUIT, Couleur.CARREAU), Cartes.P(Valeur.NEUF, Couleur.CARREAU), Cartes.P(Valeur.DIX, Couleur.CARREAU), Cartes.P(Valeur.VALET, Couleur.CARREAU))
+    val l = List[Cartes](Cartes.P(Valeur.SEPT, Couleur.CARREAU), Cartes.P(Valeur.HUIT, Couleur.CARREAU), Cartes.P(Valeur.NEUF, Couleur.CARREAU), Cartes.P(Valeur.DIX, Couleur.CARREAU), Cartes.P(Valeur.VALET, Couleur.CARREAU),Cartes.P(Valeur.VALET, Couleur.PIQUE),Cartes.P(Valeur.DAME, Couleur.PIQUE),Cartes.P(Valeur.DAME, Couleur.CARREAU))
 
-    isQuinteFlush(l) shouldBe true
-    bestHand(l).length shouldBe 5
-    bestHand(l) shouldBe l
+    val lRES = List[Cartes](Cartes.P(Valeur.HUIT, Couleur.CARREAU), Cartes.P(Valeur.NEUF, Couleur.CARREAU), Cartes.P(Valeur.DIX, Couleur.CARREAU), Cartes.P(Valeur.VALET, Couleur.CARREAU),Cartes.P(Valeur.DAME, Couleur.CARREAU))
+
+    isFlush(l,0,0,0,0) shouldBe true
+    isQuinte(l,0) shouldBe true
+    reQuinteFlush(l).length shouldBe 5
+    bestHand(l) shouldBe lRES
   }
 
   "BestHand Full" should "be defined" in {
-    val l = List[Cartes](Cartes.P(Valeur.CINQ,Couleur.CARREAU),Cartes.P(Valeur.CINQ,Couleur.COEUR),Cartes.P(Valeur.CINQ,Couleur.TREFLE),Cartes.P(Valeur.DIX,Couleur.PIQUE),Cartes.P(Valeur.DIX,Couleur.CARREAU))
+    val l = List[Cartes](Cartes.P(Valeur.CINQ,Couleur.CARREAU),Cartes.P(Valeur.CINQ,Couleur.COEUR),Cartes.P(Valeur.CINQ,Couleur.TREFLE),Cartes.P(Valeur.DIX,Couleur.PIQUE),Cartes.P(Valeur.DIX,Couleur.CARREAU),Cartes.P(Valeur.DIX,Couleur.COEUR))
+
+    val lRES = List[Cartes](Cartes.P(Valeur.CINQ,Couleur.CARREAU),Cartes.P(Valeur.CINQ,Couleur.COEUR),Cartes.P(Valeur.CINQ,Couleur.TREFLE),Cartes.P(Valeur.DIX,Couleur.CARREAU),Cartes.P(Valeur.DIX,Couleur.COEUR))
 
     isFull(l) shouldBe true
-    bestHand(l) shouldBe l
+    bestHand(l) shouldBe lRES
   }
 
   "BestHand Flush" should "be defined" in {
-    val l = List[Cartes](Cartes.P(Valeur.SEPT, Couleur.CARREAU), Cartes.P(Valeur.DEUX, Couleur.CARREAU), Cartes.P(Valeur.QUATRE, Couleur.CARREAU), Cartes.P(Valeur.DIX, Couleur.CARREAU), Cartes.P(Valeur.VALET, Couleur.CARREAU))
+    val l = List[Cartes]( Cartes.P(Valeur.DEUX, Couleur.CARREAU), Cartes.P(Valeur.QUATRE, Couleur.CARREAU),Cartes.P(Valeur.SEPT, Couleur.CARREAU), Cartes.P(Valeur.DIX, Couleur.CARREAU), Cartes.P(Valeur.VALET, Couleur.CARREAU),Cartes.P(Valeur.DAME, Couleur.CARREAU),Cartes.P(Valeur.ROI, Couleur.CARREAU))
 
     isFlush(l,0,0,0,0) shouldBe true
     reCoulFlush(l,0,0,0,0) shouldBe Couleur.CARREAU
     bestHand(l).length shouldBe 5
-    bestHand(l) shouldBe l
+    bestHand(l) shouldBe l.drop(2)
   }
 
   "BestHand Quinte" should "be defined" in {
-    val l = List[Cartes](Cartes.P(Valeur.SEPT, Couleur.CARREAU), Cartes.P(Valeur.HUIT, Couleur.COEUR), Cartes.P(Valeur.NEUF, Couleur.TREFLE), Cartes.P(Valeur.DIX, Couleur.CARREAU), Cartes.P(Valeur.VALET, Couleur.PIQUE))
+    val l = List[Cartes](Cartes.P(Valeur.SEPT, Couleur.CARREAU), Cartes.P(Valeur.HUIT, Couleur.COEUR), Cartes.P(Valeur.NEUF, Couleur.TREFLE), Cartes.P(Valeur.DIX, Couleur.CARREAU), Cartes.P(Valeur.VALET, Couleur.PIQUE),Cartes.P(Valeur.DAME, Couleur.PIQUE),Cartes.P(Valeur.ROI, Couleur.COEUR))
 
     isQuinte(l,0) shouldBe true
     bestHand(l) shouldBe l
@@ -66,13 +72,11 @@ class Tests extends AnyFlatSpec {
   }
 
   "BestHand TwoPair" should "be defined" in {
-    val l = List[Cartes](Cartes.P(Valeur.CINQ,Couleur.CARREAU),Cartes.P(Valeur.ROI,Couleur.COEUR),Cartes.P(Valeur.ROI,Couleur.TREFLE),Cartes.P(Valeur.CINQ,Couleur.PIQUE),Cartes.P(Valeur.SIX,Couleur.CARREAU))
-
-    val corre = List[Cartes](Cartes.P(Valeur.CINQ,Couleur.CARREAU),Cartes.P(Valeur.CINQ,Couleur.PIQUE),Cartes.P(Valeur.ROI,Couleur.COEUR),Cartes.P(Valeur.ROI,Couleur.TREFLE))
+    val l = List[Cartes](Cartes.P(Valeur.CINQ,Couleur.CARREAU),Cartes.P(Valeur.CINQ,Couleur.PIQUE),Cartes.P(Valeur.SIX,Couleur.CARREAU),Cartes.P(Valeur.SIX,Couleur.COEUR),Cartes.P(Valeur.ROI,Couleur.COEUR),Cartes.P(Valeur.ROI,Couleur.TREFLE))
 
     isTwoPair(l,0) shouldBe true
     bestHand(l).length shouldBe 4
-    bestHand(l) shouldBe corre
+    bestHand(l) shouldBe l.drop(2)
   }
 
   "BestHand Pair" should "be defined" in {
